ee05c74d7ea049cea7846ab63dc525955fa55b40
codemeup <laurakemp@live.co.uk>
2014-12-02 18:51:34 -0800

    so fresh so clean

--- app/books_controller.js
+++ app/books_controller.js
@@ -2,8 +2,28 @@ var bookly = angular.module("booklyApp", []);
 
 bookly.controller('BooksController', function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
-  $scope.books = books;
 
-  //Code to manage cart goes here
+  $scope.books = books;
+  $scope.predicate = 'author';
+  $scope.cart = [];
+  $scope.total=0;
+
+  $scope.addToCart = function(book){
+    $scope.cart.push(book);
+    console.log(book);
+    $scope.total= $scope.total + book.price;
+    // return cart;
+  };
+
+  $scope.plural = function(){
+
+    if ($scope.cart.length !== 1){
+      return "s";
+    }
+  };
+
+  $scope.clearcart = function(cart){
+    $scope.cart = [];
+  };
 
 });
